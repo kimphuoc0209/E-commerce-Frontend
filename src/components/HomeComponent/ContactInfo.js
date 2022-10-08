7f6d0a7cae5f0fb9f4e6aac557ea9763fb1673df
nhokshiroupro <shiroutran@gmail.com>
2022-10-08 17:32:26 +0700

    Update css, fix Route

--- src/components/HomeComponent/ContactInfo.js
+++ src/components/HomeComponent/ContactInfo.js
@@ -9,7 +9,7 @@ const ContactInfo = () => {
             <div className="info-image">
               <i className="fas fa-phone-alt"></i>
             </div>
-            <h5>Call Us 24x7</h5>
+            <h5>Phone Number</h5>
             <p>0736 230 063</p>
           </div>
         </div>
@@ -18,17 +18,17 @@ const ContactInfo = () => {
             <div className="info-image">
               <i className="fas fa-map-marker-alt"></i>
             </div>
-            <h5>Headquarter</h5>
-            <p>Arusha Njiro Pepsi</p>
+            <h5>Address</h5>
+            <p>Binh Thanh District, Ho Chi Minh City</p>
           </div>
         </div>
         <div className="col-12 col-md-4 contact-Box">
           <div className="box-info">
             <div className="info-image">
-              <i className="fas fa-fax"></i>
+              <i className="fas fa-envelope"></i>
             </div>
-            <h5>Fax</h5>
-            <p>0736 230 063</p>
+            <h5>Email</h5>
+            <p>littlefish@gmail.com</p>
           </div>
         </div>
       </div>
