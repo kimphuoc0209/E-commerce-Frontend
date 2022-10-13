497f52148c16de096481923bd5ba3e0addf4aa92
nguyentanvy <1951120161@sv.ut.edu.vn>
2022-10-13 23:35:33 +0700

    Built Profile Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.js";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomeScreen />} />
+          <Route path="/profile" element={<ProfileScreen />} />
         </Routes>
       </Router>
     </>
