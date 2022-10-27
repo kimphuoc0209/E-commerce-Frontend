3bd1c3144c1e443563b500f9222b33c2602d4766
nguyentanvy <1951120161@sv.ut.edu.vn>
2022-10-27 13:41:37 +0700

    connect redux

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <Provider store={store}>
       <App />
     </Provider>
