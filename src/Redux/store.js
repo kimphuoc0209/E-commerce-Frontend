45ebccd18c56cf5e8c431b3ca4069c57a4171558
Duong <93039763+yegiprobuild@users.noreply.github.com>
2022-10-31 15:11:31 +0700

    Integrate API to Single Product, Login, Register, Logout

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,24 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productDetailsReducer, productListReducer } from "./Reducer/ProductReducers";
+import { userLoginReducer, userRegisterReducer } from "./Reducer/userReducers";
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+}) 
 
-const initialState = {};
+//login
+const userInfoFromLocalStorage = localStorage.getItem("userInfo") 
+? JSON.parse(localStorage.getItem("userInfo"))
+: null;
+
+const initialState = {
+    userLogin: { userInfo: userInfoFromLocalStorage },
+};
 
 const middleware = [thunk];
 
@@ -13,5 +27,7 @@ const store = createStore(
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 );
+
+
 
 export default store;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux"
+import { logout } from "../Redux/Action/userActions";
+
 const Header = () => {
+  const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
   return (
     <div>
       {/* Top Header */}
@@ -35,49 +44,70 @@ const Header = () => {
       <div className="header">
         <div className="container">
           {/* MOBILE HEADER */}
-          {/* <div className="mobile-header">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-6 d-flex align-items-center">
-                                    <Link className="navbar-brand" to="/">
-                                        <img src="/images/logo.png" alt="logo"/>
-                                    </Link>
-                                </div> 
-                                <div className="col-6 d-flex align-items-center justify-content-end Login-Register">
-                                    <div className="btn-group">
-                                        <button type="button" className="name-button droptown-toggle"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i className="fas fa-user"></i>
-                                        </button>
-                                        <div className="dropdown-menu">
-                                            <Link className="dropdown-item" to="/profile">
-                                                Profile
-                                            </Link>
-                                            <Link className="dropdown-item" to="#">
-                                                Logout
-                                            </Link>
-                                        </div>
-                                    </div>
-                                    <Link className="/cart" to="cart-mobile-icon">
-                                        <i className="fas fa-shopping-cart"></i>
-                                        <span className="badge">4</span>
-                                    </Link>
-                                </div>
-                                <div className="col-12 d-flex align-items-center">
-                                    <form className="input-group">
-                                        <input
-                                            type="search"
-                                            className="form-control rounded search"
-                                             placeholder="Search"
-                                        />
-                                        <button type="submit" className="search-button">
-                                         search
-                                         </button>
-                                    </form>
-                                </div>                               
-                            </div>
-                        </div>                       
-                    </div> */}
+          <div className="mobile-header">
+            <div className="container">
+              <div className="row">
+                <div className="col-6 d-flex align-items-center">
+                  <Link className="navbar-brand" to="/">
+                    <img src="/images/logo.png" alt="logo" />
+                  </Link>
+                </div>
+                <div className="col-6 d-flex align-items-center justify-content-end Login-Register">
+                  {
+                    userInfo ? (
+                      <div className="btn-group">
+                        <button type="button" className="name-button droptown-toggle"
+                          data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <i className="fas fa-user"></i>
+                        </button>
+                        <div className="dropdown-menu">
+                          <Link className="dropdown-item" to="/profile">
+                            Profile
+                          </Link>
+                          <Link className="dropdown-item" to="#" onClick={logoutHandler}>
+                            Logout
+                          </Link>
+                        </div>
+                      </div>
+                    )
+                      :
+                      (
+                        <div className="btn-group">
+                          <button type="button" className="name-button droptown-toggle"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i className="fas fa-user"></i>
+                          </button>
+                          <div className="dropdown-menu">
+                            <Link className="dropdown-item" to="/login">
+                              Login
+                            </Link>
+                            <Link className="dropdown-item" to="/register">
+                              Register
+                            </Link>
+                          </div>
+                        </div>
+                      )
+                  }
+                  <Link className="/cart" to="cart-mobile-icon">
+                    <i className="fas fa-shopping-cart"></i>
+                    <span className="badge">4</span>
+                  </Link>
+                </div>
+                <div className="col-12 d-flex align-items-center">
+                  <form className="input-group">
+                    <input
+                      type="search"
+                      className="form-control rounded search"
+                      placeholder="Search"
+                    />
+                    <button type="submit" className="search-button">
+                      search
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
           {/* PC HEADER */}
           <div className="pc-header">
             <div className="row">
@@ -99,26 +129,45 @@ const Header = () => {
                 </form>
               </div>
               <div className="col-md-3 d-flex align-items-center justify-content-end Login-Register">
-                <div className="btn-group">
-                  <button
-                    type="button"
-                    className="name-button dropdown-toggle"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Hi, Admin Vy
-                  </button>
-                  <div className="dropdown-menu">
-                    <Link className="dropdown-item" to="/profile">
-                      Profile
-                    </Link>
 
-                    <Link className="dropdown-item" to="#">
-                      Logout
-                    </Link>
-                  </div>
-                </div>
+                {
+                  userInfo ? 
+                  (
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="name-button dropdown-toggle"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        Hi, {userInfo.name}
+                      </button>
+                      <div className="dropdown-menu">
+                        <Link className="dropdown-item" to="/profile">
+                          Profile
+                        </Link>
+
+                        <Link className="dropdown-item" to="#"
+                        onClick={logoutHandler}>
+                          Logout
+                        </Link>
+                      </div>
+                    </div>
+                  )
+                  :
+                  (
+                    <>
+                      <Link to="/register">
+                        Register
+                      </Link>
+                      <Link to="/login">
+                        Login
+                      </Link>
+                    </>  
+                  )
+                }
+
                 <Link to="/cart">
                   <i className="fas fa-shopping-bag"></i>
                   <span className="badge">4</span>
